e0455e33af4eea4fea16b3e2919359824cc807ea
Laura Schulz <lrs8810@gmail.com>
2019-11-20 11:57:45 -0700

    Refactor invoice model spec and model validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,5 @@
 class Invoice < ApplicationRecord
-  validates_presence_of :customer_id,
-                        :merchant_id,
-                        :status
+  validates_presence_of :status
 
   belongs_to :customer
   belongs_to :merchant
