972bd123ed2327ea939e4d0ba9980a47a7f5c95a
Laura Schulz <lrs8810@gmail.com>
2019-11-21 08:29:35 -0700

    Update transcaction model spec and model to validate for boolean

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,6 @@
 class Transaction < ApplicationRecord
-  validates_presence_of :credit_card_number,
-                        :result
+  validates_presence_of :credit_card_number
+  validates_inclusion_of :result, in: [true, false]
 
   belongs_to :invoice
-
-
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:credit_card_number) }
-    it { should validate_presence_of(:result) }
+    it { should_not allow_value(nil).for(:result) }
   end
 
   describe 'relationships' do
