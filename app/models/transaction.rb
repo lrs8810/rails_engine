db37f9d85f2ade4a4b1ae010ae03eb3a537d4ad8
Laura Schulz <lrs8810@gmail.com>
2019-11-22 05:04:34 -0700

    Update validation and enum on spec and model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,11 @@
 class Transaction < ApplicationRecord
-  validates_presence_of :credit_card_number
-  validates_inclusion_of :result, in: [true, false]
+  validates_presence_of :result,
+                        :credit_card_number
+
+  validates_length_of :credit_card_number, is: 16
+  validates_numericality_of :credit_card_number
 
   belongs_to :invoice
+
+  enum result: %w(success failed)
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
+    it { should validate_presence_of(:result) }
     it { should validate_presence_of(:credit_card_number) }
-    it { should_not allow_value(nil).for(:result) }
+    it { should validate_numericality_of(:credit_card_number) }
+    it { should validate_length_of(:credit_card_number).is_equal_to(16) }
   end
 
   describe 'relationships' do
     it { should belong_to :invoice }
+  end
+
+  describe 'result' do
+    it 'successful transaction' do
+      transaction = create(:transaction, result: 0)
+
+      expect(transaction.result).to eq('success')
+    end
+
+    it 'failed transaction' do
+      transaction = create(:transaction, result: 1)
+
+      expect(transaction.result).to eq('failed')
+    end
   end
 end
