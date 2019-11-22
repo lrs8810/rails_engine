0d3bbc3d6ea2d1131ca0e1d5515da04fe231cd63
Laura Schulz <lrs8810@gmail.com>
2019-11-22 08:23:10 -0700

    Add spec for factories

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to :item }
     it { should belong_to :invoice }
   end
+
+  describe 'associations' do
+    before(:each) do
+      @invoice_item = create(:invoice_item)
+    end
+
+    it 'belongs to an item' do
+      expect(@invoice_item.item).to be_a(Item)
+    end
+
+    it 'belongs to an invoice' do
+      expect(@invoice_item.invoice).to be_a(Invoice)
+    end
+  end
 end
