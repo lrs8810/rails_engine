8b1bb2ec1262e822bf7951937f3c2ce2b9006c14
Laura Schulz <lrs8810@gmail.com>
2019-11-29 20:44:47 -0700

    Add merchant/find route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      scope :merchants do
+        get '/find', to: 'merchants#find'
+      end
+
       resources :merchants, only: [:index, :show]
     end
   end
