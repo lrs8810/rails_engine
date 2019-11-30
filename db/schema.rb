a04d4c22ebba9006c2eca66c1a4ecdb1f98e3b5b
Laura Schulz <lrs8810@gmail.com>
2019-11-29 23:01:10 -0700

    Refactor first and last name columns to citext from string

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 2019_11_19_212107) do
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.citext "first_name"
+    t.citext "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
