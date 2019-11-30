8db951812e3ef7a566f86e40b466a8f79b439e51
Laura Schulz <lrs8810@gmail.com>
2019-11-29 22:21:18 -0700

    Add find action to CustomersController

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -6,10 +6,10 @@ class Api::V1::CustomersController < ApplicationController
   def show
     render json: CustomerSerializer.new(Customer.find(params[:id]))
   end
-  #
-  # def find
-  #   render json: MerchantSerializer.new(Merchant.find_by(request.query_parameters))
-  # end
+
+  def find
+    render json: CustomerSerializer.new(Customer.find_by(request.query_parameters))
+  end
   #
   # def find_all
   #   render json: MerchantSerializer.new(Merchant.where(request.query_parameters))
