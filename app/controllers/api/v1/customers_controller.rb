ef9ba37372b109e2221eb13a168d986e8a44d007
Laura Schulz <lrs8810@gmail.com>
2019-11-29 22:04:20 -0700

    Add CustomersController with index and show actions

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::CustomersController < ApplicationController
+  def index
+    render json: CustomerSerializer.new(Customer.all)
+  end
+
+  def show
+    render json: CustomerSerializer.new(Customer.find(params[:id]))
+  end
+  #
+  # def find
+  #   render json: MerchantSerializer.new(Merchant.find_by(request.query_parameters))
+  # end
+  #
+  # def find_all
+  #   render json: MerchantSerializer.new(Merchant.where(request.query_parameters))
+  # end
+  #
+  # def random
+  #   random_id = Merchant.ids.sample
+  #   render json: MerchantSerializer.new(Merchant.find(random_id))
+  # end
+end
