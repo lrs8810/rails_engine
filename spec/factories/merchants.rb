9c024762df84409ee27e05ee2d65f8d54eb424d0
Laura Schulz <lrs8810@gmail.com>
2019-12-01 14:07:38 -0700

    Add traits :with_items and :with_invoices to merchant factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :merchant do
     sequence(:name) { |i| "Dog Shop#{i}" }
+
+    trait :with_items do
+      transient do
+        items_count { 3 }
+      end
+
+      after(:create) do |merchant, evaluator|
+        merchant.items << create_list(:item, evaluator.items_count)
+      end
+    end
+
+    trait :with_invoices do
+      transient do
+        invoices_count { 3 }
+      end
+
+      after(:create) do |merchant, evaluator|
+        merchant.invoices << create_list(:invoice, evaluator.invoices_count)
+      end
+    end
   end
 end
