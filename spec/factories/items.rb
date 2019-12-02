cdfd4b23737e2ad9e1ab71e996f8fcdddc7ca9c4
Laura Schulz <lrs8810@gmail.com>
2019-12-01 17:27:25 -0700

    Add :with_invoice_items trait to item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,5 +4,15 @@ FactoryBot.define do
     sequence(:description) { |n| "This is the best bone#{n}" }
     unit_price { 350.50 }
     merchant
+
+    trait :with_invoice_items do
+      transient do
+        invoice_item_count { 3 }
+      end
+
+      after(:create) do |item, evaluator|
+        item.invoice_items << create_list(:invoice_item, evaluator.invoice_item_count)
+      end
+    end
   end
 end
