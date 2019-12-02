3fce4f951a0ccea1a3cb3ce4c014a88c532a3cc4
Laura Schulz <lrs8810@gmail.com>
2019-12-01 18:33:15 -0700

    Add traits with_invoices to customer factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :customer do
     sequence(:first_name) { |i| "Laura#{i}" }
     sequence(:last_name) { |i| "Schulz#{i}" }
+
+    trait :with_invoices do
+      transient do
+        invoice_count { 3 }
+      end
+
+      after(:create) do |customer, evaluator|
+        customer.invoices << create_list(:invoice, evaluator.invoice_count)
+      end
+    end
   end
 end
